7e55f0cc9fd5ec897435fe9afdc66f59620ebd4a
Viktor-1995 <madebyvityok@ukr.net>
2025-11-25 23:22:49 +0100

    1.0 solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const el = document.querySelector('ul');
+const largeImg = document.getElementById('largeImg');
+
+el.addEventListener('click', ChangeImg);
+
+function ChangeImg(e) {
+  e.preventDefault();
+
+  if (!e.target.src) {
+    largeImg.src = e.target.querySelector('img').src;
+  } else {
+    largeImg.src = e.target.src;
+  }
+}
