2fc8b87c8b6c653a8b465748e3caff4f3734cff9
Viktor-1995 <madebyvityok@ukr.net>
2025-11-25 23:51:58 +0100

    make build

---
+++ index.fb1115ce.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("ul"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault(),e.target.src?t.src=e.target.src:t.src=e.target.querySelector("img").src});
+//# sourceMappingURL=index.fb1115ce.js.map
